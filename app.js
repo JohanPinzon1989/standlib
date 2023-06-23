c2c7bf18ac52a9b6462faaa964c267f457e845f2
Johan Enrique Pinzon Rodriguez <johan.pinzon1989@gmail.com>
2023-06-22 22:40:33 -0500

    Iniciamos de nuevo

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookiesParse = require("cookie-parser");
+
+const app = express();
+
+// Setar motor de plantillas
+app.set("view engine", "ejs");
+
+//Setear carpeta public para archivos externos
+app.use(express.static("public"));
+
+//Procesar datos enviados desde formularios
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Setear variables de entorno
+dotenv.config({ path: "./env/.env" });
+
+//Setear las cookies
+//app.use(cookiesParse);
+
+//Lammar al router
+app.use("/", require("./routes/router"));
+
+app.listen(4000, () => {
+  console.log("Server up running in port: 4000");
+});
