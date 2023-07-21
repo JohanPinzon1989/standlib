c03b5a8596432bbeffebcfdb7855db68a875e456
Johan Enrique Pinzon Rodriguez <46166600+JohanPinzon1989@users.noreply.github.com>
2023-07-20 21:22:03 -0500

    .

--- cliente/app.js
+++ cliente/app.js
@@ -4,28 +4,28 @@ const dotenv = require("dotenv");
 const cookiesParse = require("cookie-parser");
 const path = require("path");
 const cors = require("cors");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const { mysql } = require("./config");
-const myconnection = require('express-myconnection');
-const mysql2 = require('mysql2');
-const routers = require('./routes/router');
+const myconnection = require("express-myconnection");
+const mysql2 = require("mysql2");
+const routers = require("./routes/router");
 
 const app = express();
-
-// Setar motor de plantillas
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 
 //Setear carpeta public para archivos externos
 app.use(express.static("public/"));
 
+// Setar motor de plantillas
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 //Procesar datos enviados desde formularios
-app.use(cors());
 app.use(
   bodyParser.urlencoded({
     extended: true,
-  }),
+  })
 );
+app.use(cors());
 app.use(bodyParser.json());
 //app.use(express.urlencoded({ extended: true }));
 //app.use(express.json());
@@ -36,14 +36,20 @@ dotenv.config();
 //Setear las cookies
 app.use(cookiesParse());
 
-app.use(myconnection(mysql2, {
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASS,
-  database: process.env.DATABASE,
-  port: 3306,
-  insecureAuth: true,
-}, 'single'));
+app.use(
+  myconnection(
+    mysql2,
+    {
+      host: process.env.HOST,
+      user: process.env.USER,
+      password: process.env.PASS,
+      database: process.env.DATABASE,
+      port: 3306,
+      insecureAuth: true,
+    },
+    "single"
+  )
+);
 
 //Lammar al router
 app.use("/", routers);
@@ -51,6 +57,5 @@ app.use("/", routers);
 const port = process.env.PORT_SER;
 
 app.listen(port || 4000, () => {
-  console.log("Server up running: "+ port);
+  console.log("Server up running: " + port);
 });
-
